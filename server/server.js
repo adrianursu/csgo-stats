7f86b5f9dddf839ba6210de0f90c2419cb981fc6
Ursu Adrian <uursu.adrian@gmail.com>
2023-03-22 21:48:16 +0100

    fetching the url and writting in .txt

--- server/server.js
+++ server/server.js
@@ -1,13 +1,41 @@
 const express = require("express");
+const fs = require("fs");
+const http = require("http");
+const https = require("https");
 const app = express();
-
 const port = 8080;
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const URL =
+  "https://blast-recruiting.s3.eu-central-1.amazonaws.com/NAVIvsVitaGF-Nuke.txt";
+
+app.get("", (req, res) => {
+  const protocol = URL.startsWith("https") ? https : http;
+
+  protocol
+    .get(URL, (response) => {
+      let data = "";
+
+      response.on("data", (chunk) => {
+        data += chunk;
+      });
+
+      response.on("end", () => {
+        fs.writeFile("NAVIvsVitaGF.txt", data, (err) => {
+          if (err) throw err;
+          console.log("File created and data written to it!");
+
+          fs.readFile("NAVIvsVitaGF.txt", "utf8", (err, data) => {
+            if (err) throw err;
+            console.log(data);
+          });
+        });
+      });
+    })
+    // eslint-disable-next-line no-unused-vars
+    .on("error", (error) => {
+      res.status(500).send("Error fetching URL");
+    });
 });
+
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
-
-// const URL =
-//   "https://blast-recruiting.s3.eu-central-1.amazonaws.com/NAVIvsVitaGF-Nuke.txt";
