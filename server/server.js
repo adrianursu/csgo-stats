f5194efa7dc84d53e13794c60e937d89a5dbd4d1
Ursu Adrian <uursu.adrian@gmail.com>
2023-03-26 15:38:38 +0200

    covering all the cases when creating a file and writing the data

--- server/server.js
+++ server/server.js
@@ -7,26 +7,40 @@ const port = 8080;
 const URL =
   "https://blast-recruiting.s3.eu-central-1.amazonaws.com/NAVIvsVitaGF-Nuke.txt";
 
+// fs.appendFile("NAVIvsVitaGF.txt", "", (err) => {
+//   if (err) throw err;
+//   console.log("Created File!");
+// });
+
 app.get("", (req, res) => {
   const protocol = URL.startsWith("https") ? https : http;
 
   protocol
     .get(URL, (response) => {
-      let data = "";
+      let urlData = "";
 
       response.on("data", (chunk) => {
-        data += chunk;
+        urlData += chunk;
       });
 
       response.on("end", () => {
-        fs.writeFile("NAVIvsVitaGF.txt", data, (err) => {
-          if (err) throw err;
-          console.log("File created and data written to it!");
-
-          fs.readFile("NAVIvsVitaGF.txt", "utf8", (err, data) => {
-            if (err) throw err;
-            console.log(data);
-          });
+        fs.readFile("NAVIvsVitaGF.txt", "utf8", (err, data) => {
+          if (err) {
+            fs.appendFile("NAVIvsVitaGF.txt", "", (err) => {
+              if (err) throw err;
+              console.log("Created File!");
+            });
+          }
+          if (data === urlData) {
+            console.log("Data is already updated.");
+            res.send("Data is already updated.");
+          } else {
+            fs.writeFile("NAVIvsVitaGF.txt", urlData, (err) => {
+              if (err) throw err;
+              console.log("File has been created and data was written in it");
+              res.send("File has been created and data was written in it");
+            });
+          }
         });
       });
     })
