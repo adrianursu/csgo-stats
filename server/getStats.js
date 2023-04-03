85f269c1697271c9da71a49fa4812723d2f8508c
Ursu Adrian <uursu.adrian@gmail.com>
2023-04-03 21:26:44 +0200

    created regex and function for getting deaths of a player

--- server/getStats.js
+++ server/getStats.js
@@ -1,14 +1,34 @@
 const fs = require("fs");
 
-const getKills = (player) => {
-  return new RegExp(`\\b${player}\\b.*killed(?!.*other)\\b`, "g");
-};
-
 fs.readFile("NAVIvsVitaGF.txt", "utf-8", (err, data) => {
   if (err) {
     console.log("Error reading file");
   }
 
-  const array = [...data.matchAll(getKills("b1t"))];
-  console.log(array.length);
+  const getKills = (player) => {
+    const regex = new RegExp(`\\b${player}\\b.*killed(?!.*other)\\b`, "g");
+    const killsRegexMatch = [...data.matchAll(regex)];
+    console.log(
+      "Player " +
+        player +
+        " has killed " +
+        killsRegexMatch.length +
+        " opponents"
+    );
+  };
+
+  const getDeaths = (player) => {
+    const regex = new RegExp(`killed\\s+".*${player}.*"`, "g");
+    const deathsRegexMatch = [...data.matchAll(regex)];
+    console.log(
+      "Player " +
+        player +
+        " has been killed for " +
+        deathsRegexMatch.length +
+        " times during this match"
+    );
+  };
+
+  getKills("s1mple");
+  getDeaths("s1mple");
 });
