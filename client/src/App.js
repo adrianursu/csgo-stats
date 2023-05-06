dde6cf079bdf4116052b5a2b4f27847d9abf5157
Ursu Adrian <uursu.adrian@gmail.com>
2023-05-06 14:33:32 +0200

    create-react-app

---
+++ client/src/App.js
@@ -0,0 +1,7 @@
+import "./index.css";
+
+function App() {
+  return <p>Client MVP</p>;
+}
+
+export default App;
--- client/src/app/page.js
+++
@@ -1,9 +0,0 @@
-import "./globals.css";
-
-export default function Home() {
-  return (
-    <>
-      <p>Getting started with Client part MVP</p>
-    </>
-  );
-}
